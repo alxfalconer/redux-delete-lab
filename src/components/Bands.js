4c4f9dd3206450f2535d0a2e8b2c82b4a22df9d3
Alexander <al.x.falconer@gmail.com>
2022-08-06 00:05:29 -0400

    bands component

---
+++ src/components/Bands.js
@@ -0,0 +1,23 @@
+import Band from './Band';
+
+class Bands extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+
+  render() {
+
+    let bands = this.props.bands.map(band => <Band band={band} key={band.id} delete={this.props.delete}/>);
+
+    return (
+      <div>
+        {bands}
+      </div>
+    );
+  }
+
+}
+
+export default Bands;
